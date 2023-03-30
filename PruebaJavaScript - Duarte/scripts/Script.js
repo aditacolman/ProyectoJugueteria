5964ba29fbf87a22f2a38004272d543c37f3c569
xaDuar <108692819+xaDuar@users.noreply.github.com>
2023-03-30 14:55:33 -0300

    Add files via upload

---
+++ PruebaJavaScript - Duarte/scripts/Script.js
@@ -0,0 +1,21 @@
+let ellet = null
+const declarounaconstante = "chau"
+var strinaleatorio = "hola"
+var booleano = true
+var indefinido = undefined
+let simbolo = Symbol('holis')
+var variablenumerica = 32
+var miCasa = new Object();
+var num8 = 4
+var num16 = 16
+
+miCasa.techo = "con goteras";
+miCasa.aireAcon = "no tiene";
+miCasa.comida = 0;
+
+console.log(strinaleatorio)
+console.log(num8 + num16)
+console.log(num16 - num8)
+console.log(num16 / num8)
+console.log(num16 * num8)
+console.log(num16 ** num8)
